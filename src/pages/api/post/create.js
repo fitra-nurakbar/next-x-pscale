305aaf9a6db94f827936934930418202e5de9812
nando8 <fitranurakbar378@gmail.com>
2022-10-28 00:37:49 +0700

    connection to pscale

---
+++ src/pages/api/post/create.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    return await createPost(req, res)
+  } else {
+    return res.status(405).json({ message: 'Method not allowed', success: false })
+  }
+}
+
+async function createPost(req, res) {
+  const body = req.body
+  try {
+    const newPost = await prisma.Posts.create({
+      data: {
+        title: body.title,
+        post: body.post,
+      }
+    })
+    return res.status(200).json(newPost, { success: true })
+  } catch (error) {
+    console.error('Request error', error)
+    res.status(500).json({ error: 'Error creating post', success: false })
+  }
+}
---
+++ src/pages/api/post/index.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    return await getPosts(req, res)
+  } else {
+    return res.status(405).json({ message: 'Method not allowed', success: false })
+  }
+}
+
+async function getPosts(req, res) {
+  const body = req.body
+  try {
+    const allPosts = await prisma.Posts.findMany()
+    return res.status(200).json(allPosts, { success: true })
+  } catch (error) {
+    console.error('Request error', error)
+    res.status(500).json({ error: 'Error getting posts', success: false })
+  }
+}
